e21b8922cf1d5ae2c873f394fb7aff43ed119651
KamilDA <69484177+KamilDA@users.noreply.github.com>
2020-09-15 11:01:57 -0400

    Framecounter update

--- Build/KamilDA.github.io.framework.js
+++ Build/KamilDA.github.io.framework.js
@@ -1299,7 +1299,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3335296;
+STATICTOP = STATIC_BASE + 3394832;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3229,7 +3229,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3335296;
+var STATIC_BUMP = 3394832;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -3340,9 +3340,195 @@ var WEBAudio = {
  audioContext: {},
  audioWebEnabled: 0
 };
+function _JS_Sound_Create_Channel(callback, userData) {
+ if (WEBAudio.audioWebEnabled == 0) return;
+ var channel = {
+  gain: WEBAudio.audioContext.createGain(),
+  panner: WEBAudio.audioContext.createPanner(),
+  threeD: false,
+  playBuffer: (function(delay, buffer, offset) {
+   this.source.buffer = buffer;
+   var chan = this;
+   this.source.onended = (function() {
+    if (callback) dynCall("vi", callback, [ userData ]);
+    chan.setup();
+   });
+   this.source.start(delay, offset);
+  }),
+  setup: (function() {
+   this.source = WEBAudio.audioContext.createBufferSource();
+   this.setupPanning();
+  }),
+  setupPanning: (function() {
+   if (this.threeD) {
+    this.source.disconnect();
+    this.source.connect(this.panner);
+    this.panner.connect(this.gain);
+   } else {
+    this.panner.disconnect();
+    this.source.connect(this.gain);
+   }
+  })
+ };
+ channel.panner.rolloffFactor = 0;
+ channel.gain.connect(WEBAudio.audioContext.destination);
+ channel.setup();
+ return WEBAudio.audioInstances.push(channel) - 1;
+}
+function _JS_Sound_GetLength(bufferInstance) {
+ if (WEBAudio.audioWebEnabled == 0) return 0;
+ var sound = WEBAudio.audioInstances[bufferInstance];
+ var sampleRateRatio = 44100 / sound.buffer.sampleRate;
+ return sound.buffer.length * sampleRateRatio;
+}
+function _JS_Sound_GetLoadState(bufferInstance) {
+ if (WEBAudio.audioWebEnabled == 0) return 2;
+ var sound = WEBAudio.audioInstances[bufferInstance];
+ if (sound.error) return 2;
+ if (sound.buffer) return 0;
+ return 1;
+}
+function _JS_Sound_Init() {
+ try {
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  WEBAudio.audioContext = new AudioContext;
+  var tryToResumeAudioContext = (function() {
+   if (WEBAudio.audioContext.state === "suspended") WEBAudio.audioContext.resume(); else Module.clearInterval(resumeInterval);
+  });
+  var resumeInterval = Module.setInterval(tryToResumeAudioContext, 400);
+  WEBAudio.audioWebEnabled = 1;
+ } catch (e) {
+  alert("Web Audio API is not supported in this browser");
+ }
+}
+function _JS_Sound_Load(ptr, length) {
+ if (WEBAudio.audioWebEnabled == 0) return 0;
+ var sound = {
+  buffer: null,
+  error: false
+ };
+ var instance = WEBAudio.audioInstances.push(sound) - 1;
+ var audioData = HEAPU8.buffer.slice(ptr, ptr + length);
+ WEBAudio.audioContext.decodeAudioData(audioData, (function(buffer) {
+  sound.buffer = buffer;
+ }), (function() {
+  sound.error = true;
+  console.log("Decode error.");
+ }));
+ return instance;
+}
+function _JS_Sound_Load_PCM(channels, length, sampleRate, ptr) {
+ if (WEBAudio.audioWebEnabled == 0) return 0;
+ var sound = {
+  buffer: WEBAudio.audioContext.createBuffer(channels, length, sampleRate),
+  error: false
+ };
+ for (var i = 0; i < channels; i++) {
+  var offs = (ptr >> 2) + length * i;
+  var buffer = sound.buffer;
+  var copyToChannel = buffer["copyToChannel"] || (function(source, channelNumber, startInChannel) {
+   var clipped = source.subarray(0, Math.min(source.length, this.length - (startInChannel | 0)));
+   this.getChannelData(channelNumber | 0).set(clipped, startInChannel | 0);
+  });
+  copyToChannel.apply(buffer, [ HEAPF32.subarray(offs, offs + length), i, 0 ]);
+ }
+ var instance = WEBAudio.audioInstances.push(sound) - 1;
+ return instance;
+}
+function _JS_Sound_Play(bufferInstance, channelInstance, offset, delay) {
+ _JS_Sound_Stop(channelInstance, 0);
+ if (WEBAudio.audioWebEnabled == 0) return;
+ var sound = WEBAudio.audioInstances[bufferInstance];
+ var channel = WEBAudio.audioInstances[channelInstance];
+ if (sound.buffer) {
+  try {
+   channel.playBuffer(WEBAudio.audioContext.currentTime + delay, sound.buffer, offset);
+  } catch (e) {
+   console.error("playBuffer error. Exception: " + e);
+  }
+ } else console.log("Trying to play sound which is not loaded.");
+}
+function _JS_Sound_ReleaseInstance(instance) {
+ WEBAudio.audioInstances[instance] = null;
+}
 function _JS_Sound_ResumeIfNeeded() {
  if (WEBAudio.audioWebEnabled == 0) return;
  if (WEBAudio.audioContext.state === "suspended") WEBAudio.audioContext.resume();
+}
+function _JS_Sound_Set3D(channelInstance, threeD) {
+ var channel = WEBAudio.audioInstances[channelInstance];
+ if (channel.threeD != threeD) {
+  channel.threeD = threeD;
+  channel.setupPanning();
+ }
+}
+function _JS_Sound_SetListenerOrientation(x, y, z, xUp, yUp, zUp) {
+ if (WEBAudio.audioWebEnabled == 0) return;
+ if (WEBAudio.audioContext.listener.forwardX) {
+  WEBAudio.audioContext.listener.forwardX.setValueAtTime(-x, WEBAudio.audioContext.currentTime);
+  WEBAudio.audioContext.listener.forwardY.setValueAtTime(-y, WEBAudio.audioContext.currentTime);
+  WEBAudio.audioContext.listener.forwardZ.setValueAtTime(-z, WEBAudio.audioContext.currentTime);
+  WEBAudio.audioContext.listener.upX.setValueAtTime(xUp, WEBAudio.audioContext.currentTime);
+  WEBAudio.audioContext.listener.upY.setValueAtTime(yUp, WEBAudio.audioContext.currentTime);
+  WEBAudio.audioContext.listener.upZ.setValueAtTime(zUp, WEBAudio.audioContext.currentTime);
+ } else {
+  WEBAudio.audioContext.listener.setOrientation(-x, -y, -z, xUp, yUp, zUp);
+ }
+}
+function _JS_Sound_SetListenerPosition(x, y, z) {
+ if (WEBAudio.audioWebEnabled == 0) return;
+ if (WEBAudio.audioContext.listener.positionX) {
+  WEBAudio.audioContext.listener.positionX.setValueAtTime(x, WEBAudio.audioContext.currentTime);
+  WEBAudio.audioContext.listener.positionY.setValueAtTime(y, WEBAudio.audioContext.currentTime);
+  WEBAudio.audioContext.listener.positionZ.setValueAtTime(z, WEBAudio.audioContext.currentTime);
+ } else {
+  WEBAudio.audioContext.listener.setPosition(x, y, z);
+ }
+}
+function _JS_Sound_SetLoop(channelInstance, loop) {
+ if (WEBAudio.audioWebEnabled == 0) return;
+ WEBAudio.audioInstances[channelInstance].source.loop = loop;
+}
+function _JS_Sound_SetLoopPoints(channelInstance, loopStart, loopEnd) {
+ if (WEBAudio.audioWebEnabled == 0) return;
+ var channel = WEBAudio.audioInstances[channelInstance];
+ channel.source.loopStart = loopStart;
+ channel.source.loopEnd = loopEnd;
+}
+function _JS_Sound_SetPitch(channelInstance, v) {
+ if (WEBAudio.audioWebEnabled == 0) return;
+ try {
+  WEBAudio.audioInstances[channelInstance].source.playbackRate.setValueAtTime(v, WEBAudio.audioContext.currentTime);
+ } catch (e) {
+  console.error("Invalid audio pitch " + v + " specified to WebAudio backend!");
+ }
+}
+function _JS_Sound_SetPosition(channelInstance, x, y, z) {
+ if (WEBAudio.audioWebEnabled == 0) return;
+ WEBAudio.audioInstances[channelInstance].panner.setPosition(x, y, z);
+}
+function _JS_Sound_SetVolume(channelInstance, v) {
+ if (WEBAudio.audioWebEnabled == 0) return;
+ try {
+  WEBAudio.audioInstances[channelInstance].gain.gain.setValueAtTime(v, WEBAudio.audioContext.currentTime);
+ } catch (e) {
+  console.error("Invalid audio volume " + v + " specified to WebAudio backend!");
+ }
+}
+function _JS_Sound_Stop(channelInstance, delay) {
+ if (WEBAudio.audioWebEnabled == 0) return;
+ var channel = WEBAudio.audioInstances[channelInstance];
+ if (channel.source.buffer) {
+  try {
+   channel.source.stop(WEBAudio.audioContext.currentTime + delay);
+  } catch (e) {
+   channel.source.disconnect();
+  }
+  if (delay == 0) {
+   channel.source.onended = (function() {});
+   channel.setup();
+  }
+ }
 }
 function _JS_SystemInfo_GetCanvasClientSize(domElementSelector, outWidth, outHeight) {
  var selector = UTF8ToString(domElementSelector);
@@ -13242,6 +13428,12 @@ var _llvm_fabs_f32 = Math_abs;
 var _llvm_fabs_f64 = Math_abs;
 var _llvm_floor_f32 = Math_floor;
 var _llvm_floor_f64 = Math_floor;
+function _llvm_log10_f32(x) {
+ return Math.log(x) / Math.LN10;
+}
+function _llvm_log10_f64() {
+ return _llvm_log10_f32.apply(null, arguments);
+}
 function _llvm_log2_f32(x) {
  return Math.log(x) / Math.LN2;
 }
@@ -14804,6 +14996,11 @@ function nullFunc_vffi(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
+function nullFunc_vffii(x) {
+ err("Invalid function pointer called with signature 'vffii'. Perhaps this is an invalid value (e.g. caused by calling a virtual method on a NULL pointer)? Or calling a function with an incorrect type, which will fail? (it is worth building your source files with -Werror (warnings are errors), as warnings can indicate undefined behavior which can cause this)");
+ err("Build with ASSERTIONS=2 for more info.");
+ abort(x);
+}
 function nullFunc_vfi(x) {
  err("Invalid function pointer called with signature 'vfi'. Perhaps this is an invalid value (e.g. caused by calling a virtual method on a NULL pointer)? Or calling a function with an incorrect type, which will fail? (it is worth building your source files with -Werror (warnings are errors), as warnings can indicate undefined behavior which can cause this)");
  err("Build with ASSERTIONS=2 for more info.");
@@ -15464,8 +15661,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 97848;
-Module["wasmMaxTableSize"] = 97848;
+Module["wasmTableSize"] = 98118;
+Module["wasmMaxTableSize"] = 98118;
 function invoke_d(index) {
  var sp = stackSave();
  try {
@@ -17116,6 +17313,16 @@ function invoke_vffi(index, a1, a2, a3) {
   Module["setThrew"](1, 0);
  }
 }
+function invoke_vffii(index, a1, a2, a3, a4) {
+ var sp = stackSave();
+ try {
+  Module["dynCall_vffii"](index, a1, a2, a3, a4);
+ } catch (e) {
+  stackRestore(sp);
+  if (typeof e !== "number" && e !== "longjmp") throw e;
+  Module["setThrew"](1, 0);
+ }
+}
 function invoke_vfi(index, a1, a2) {
  var sp = stackSave();
  try {
@@ -18609,6 +18816,7 @@ Module.asmLibraryArg = {
  "nullFunc_vffffi": nullFunc_vffffi,
  "nullFunc_vffffii": nullFunc_vffffii,
  "nullFunc_vffi": nullFunc_vffi,
+ "nullFunc_vffii": nullFunc_vffii,
  "nullFunc_vfi": nullFunc_vfi,
  "nullFunc_vfii": nullFunc_vfii,
  "nullFunc_vfiii": nullFunc_vfiii,
@@ -18906,6 +19114,7 @@ Module.asmLibraryArg = {
  "invoke_vffffi": invoke_vffffi,
  "invoke_vffffii": invoke_vffffii,
  "invoke_vffi": invoke_vffi,
+ "invoke_vffii": invoke_vffii,
  "invoke_vfi": invoke_vfi,
  "invoke_vfii": invoke_vfii,
  "invoke_vfiii": invoke_vfiii,
@@ -19048,7 +19257,24 @@ Module.asmLibraryArg = {
  "_JS_Log_Dump": _JS_Log_Dump,
  "_JS_Log_StackTrace": _JS_Log_StackTrace,
  "_JS_Profiler_InjectJobs": _JS_Profiler_InjectJobs,
+ "_JS_Sound_Create_Channel": _JS_Sound_Create_Channel,
+ "_JS_Sound_GetLength": _JS_Sound_GetLength,
+ "_JS_Sound_GetLoadState": _JS_Sound_GetLoadState,
+ "_JS_Sound_Init": _JS_Sound_Init,
+ "_JS_Sound_Load": _JS_Sound_Load,
+ "_JS_Sound_Load_PCM": _JS_Sound_Load_PCM,
+ "_JS_Sound_Play": _JS_Sound_Play,
+ "_JS_Sound_ReleaseInstance": _JS_Sound_ReleaseInstance,
  "_JS_Sound_ResumeIfNeeded": _JS_Sound_ResumeIfNeeded,
+ "_JS_Sound_Set3D": _JS_Sound_Set3D,
+ "_JS_Sound_SetListenerOrientation": _JS_Sound_SetListenerOrientation,
+ "_JS_Sound_SetListenerPosition": _JS_Sound_SetListenerPosition,
+ "_JS_Sound_SetLoop": _JS_Sound_SetLoop,
+ "_JS_Sound_SetLoopPoints": _JS_Sound_SetLoopPoints,
+ "_JS_Sound_SetPitch": _JS_Sound_SetPitch,
+ "_JS_Sound_SetPosition": _JS_Sound_SetPosition,
+ "_JS_Sound_SetVolume": _JS_Sound_SetVolume,
+ "_JS_Sound_Stop": _JS_Sound_Stop,
  "_JS_SystemInfo_GetCanvasClientSize": _JS_SystemInfo_GetCanvasClientSize,
  "_JS_SystemInfo_GetDocumentURL": _JS_SystemInfo_GetDocumentURL,
  "_JS_SystemInfo_GetGPUInfo": _JS_SystemInfo_GetGPUInfo,
@@ -19391,6 +19617,8 @@ Module.asmLibraryArg = {
  "_llvm_fabs_f64": _llvm_fabs_f64,
  "_llvm_floor_f32": _llvm_floor_f32,
  "_llvm_floor_f64": _llvm_floor_f64,
+ "_llvm_log10_f32": _llvm_log10_f32,
+ "_llvm_log10_f64": _llvm_log10_f64,
  "_llvm_log2_f32": _llvm_log2_f32,
  "_llvm_pow_f64": _llvm_pow_f64,
  "_llvm_sin_f32": _llvm_sin_f32,
@@ -26100,6 +26328,11 @@ var dynCall_vffi = Module["dynCall_vffi"] = (function() {
  assert(runtimeInitialized, "you need to wait for the runtime to be ready (e.g. wait for main() to be called)");
  assert(!runtimeExited, "the runtime was exited (use NO_EXIT_RUNTIME to keep it alive after main() exits)");
  return Module["asm"]["dynCall_vffi"].apply(null, arguments);
+});
+var dynCall_vffii = Module["dynCall_vffii"] = (function() {
+ assert(runtimeInitialized, "you need to wait for the runtime to be ready (e.g. wait for main() to be called)");
+ assert(!runtimeExited, "the runtime was exited (use NO_EXIT_RUNTIME to keep it alive after main() exits)");
+ return Module["asm"]["dynCall_vffii"].apply(null, arguments);
 });
 var dynCall_vfi = Module["dynCall_vfi"] = (function() {
  assert(runtimeInitialized, "you need to wait for the runtime to be ready (e.g. wait for main() to be called)");
