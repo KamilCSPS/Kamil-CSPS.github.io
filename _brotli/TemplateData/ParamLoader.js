2b774c0537abafc57ca61e7b349262cf09803399
KamilDA <69484177+KamilDA@users.noreply.github.com>
2020-09-21 14:13:36 -0400

    Production build without MSAA

--- _brotli/TemplateData/ParamLoader.js
+++
@@ -1,7 +0,0 @@
-// Call using: GetUrlVars()['xxx'];
-
-function GetUrlVars(){
-	var vars = {};
-	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value){ vars[key] = value;});
-	return vars;
-}
